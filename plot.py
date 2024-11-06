3b3d0ded5be8c1cfea19fa0448d285ef2fbaab16
Jens Krumsieck <j.krumsieck@outlook.de>
2024-11-06 08:52:59 +0000

    added argparse

--- plot.py
+++ plot.py
@@ -1,11 +1,16 @@
 import matplotlib
 from matplotlib import pyplot as plt
 import pandas as pd
+import argparse
 import scienceplots
 assert scienceplots, 'scienceplots needed'
 plt.style.use(['science', 'no-latex'])
 
-df = pd.read_csv('results.csv')
+parser = argparse.ArgumentParser(prog="python plot.py", description="Plots the percentage of speakers for each language")
+parser.add_argument("-r", "--results", required=True, help="Path to the results.csv File")
+args = parser.parse_args()
+
+df = pd.read_csv(args.results)
 colors = matplotlib.colormaps['tab10'](range(len(df)))
 
 ax = df.plot.bar(x='language', y='percentage', legend=False, title='Language Popularity', color=colors) 
